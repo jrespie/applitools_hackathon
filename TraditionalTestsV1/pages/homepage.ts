5ed37502ba07899b9eb32311fdbf3be35199d4be
James Espie <jamesespie@faceme.com>
2020-07-07 22:48:46 +1200

    Added more checks for Task 01 traditional

--- TraditionalTestsV1/pages/homepage.ts
+++ TraditionalTestsV1/pages/homepage.ts
@@ -5,11 +5,26 @@ class HomePage extends BasePage {
         super.open('gridHackathonV1.html');
     }
 
-    applifashionSearchBarLocator = '#INPUTtext____42';
-    applifashionHeaderSubMenuLocator = '#A__showsubmen__23';
+    searchBarLocator = '#INPUTtext____42';
+    headerSubMenuLocator = '#A__showsubmen__23';
+    sidebarFiltersLocator = '#sidebar_filters';
+    wishlistIconLocator = '#A__wishlist__52';
+    gridviewIconLocator = '#I__tiviewgrid__202';
+    listviewIconLocator = '#I__tiviewlist__204';
+    openFiltersIconLocator = '#ti-filter';
+    filtersLabelLocator = '#SPAN____208';
+    productShortcutsLocator = '#UL____222';
 
-    get applifashionSearchBar() { return $(this.applifashionSearchBarLocator) }
-    get applifashionHeaderSubMenu() { return $(this.applifashionHeaderSubMenuLocator) }
+    get searchBar() { return $(this.searchBarLocator) }
+    get headerSubMenu() { return $(this.headerSubMenuLocator) }
+    get sidebarFilters() { return $(this.sidebarFiltersLocator) }
+    get wishlistIcon() { return $(this.wishlistIconLocator) }
+    get gridViewIcon() { return $(this.gridviewIconLocator) }
+    get listViewIcon() { return $(this.listviewIconLocator) }
+    get openFiltersIcon() { return $(this.openFiltersIconLocator) }
+    get filtersLabel() { return $(this.filtersLabelLocator) }
+    get productShortcuts() { return $(this.productShortcutsLocator) }
+
 }
 
 export default new HomePage();
--- TraditionalTestsV1/specs/Task01-spec.ts
+++ TraditionalTestsV1/specs/Task01-spec.ts
@@ -18,20 +18,20 @@ describe('Open Homepage', () => {
     describe('search bar element checks', function() {
         it('search bar should be present in desktop view', async function() {
             let expectedResult = true;
-            let result = await checkForElementVisibility(HomePage.applifashionSearchBar, 'DESKTOP', this.test?.title, HomePage.applifashionSearchBarLocator, expectedResult);
+            let result = await checkForElementVisibility(HomePage.searchBar, 'DESKTOP', this.test?.title, HomePage.searchBarLocator, expectedResult);
 
             expect(result).to.equal(true);
         });
 
         it('search bar should be present in tablet view', async function() {
             let expectedResult=true;
-            let result = await checkForElementVisibility(HomePage.applifashionSearchBar, 'TABLET', this.test?.title, HomePage.applifashionSearchBarLocator, expectedResult);
+            let result = await checkForElementVisibility(HomePage.searchBar, 'TABLET', this.test?.title, HomePage.searchBarLocator, expectedResult);
 
             expect(result).to.equal(true);
         });
         it('search bar should not be present in mobile view', async function() {
             let expectedResult = false;
-            let result = await checkForElementVisibility(HomePage.applifashionSearchBar, 'MOBILE', this.test?.title, HomePage.applifashionSearchBarLocator, expectedResult);
+            let result = await checkForElementVisibility(HomePage.searchBar, 'MOBILE', this.test?.title, HomePage.searchBarLocator, expectedResult);
 
             expect(result).to.equal(true);
         });
@@ -40,24 +40,169 @@ describe('Open Homepage', () => {
     describe('submenu element checks',() => {
         it('submenu should be present in desktop view', async function() {
             let expectedResult = true;
-            let result = await checkForElementVisibility(HomePage.applifashionHeaderSubMenu, 'DESKTOP', this.test?.title, HomePage.applifashionHeaderSubMenuLocator, expectedResult);
+            let result = await checkForElementVisibility(HomePage.headerSubMenu, 'DESKTOP', this.test?.title, HomePage.headerSubMenuLocator, expectedResult);
 
             expect(result).to.equal(true);
         });
         it('submenu should not be present in tablet view', async function() {
             let expectedResult = false;
-            let result = await checkForElementVisibility(HomePage.applifashionHeaderSubMenu, 'TABLET', this.test?.title, HomePage.applifashionHeaderSubMenuLocator, expectedResult);
+            let result = await checkForElementVisibility(HomePage.headerSubMenu, 'TABLET', this.test?.title, HomePage.headerSubMenuLocator, expectedResult);
 
             expect(result).to.equal(true);
         });
         it('submenu should not be present in mobile view', async function() {
             let expectedResult = false;
-            let result = await checkForElementVisibility(HomePage.applifashionHeaderSubMenu, 'MOBILE', this.test?.title, HomePage.applifashionHeaderSubMenuLocator, expectedResult);
+            let result = await checkForElementVisibility(HomePage.headerSubMenu, 'MOBILE', this.test?.title, HomePage.headerSubMenuLocator, expectedResult);
 
             expect(result).to.equal(true);
         });
     })
 
+    describe('sidebar filter element checks', () => {
+        it('sidebar filter should be present in desktop view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.sidebarFilters, 'DESKTOP', this.test?.title, HomePage.sidebarFiltersLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('sidebar filter should not be present in tablet view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.sidebarFilters, 'TABLET', this.test?.title, HomePage.sidebarFiltersLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('sidebar filter should not be present in mobile view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.sidebarFilters, 'MOBILE', this.test?.title, HomePage.sidebarFiltersLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    })
+
+    describe('wishlist icon element checks', () => {
+        it('wishlist icon should be present in desktop view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.wishlistIcon, 'DESKTOP', this.test?.title, HomePage.wishlistIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('wishlist icon should not be present in tablet view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.wishlistIcon, 'TABLET', this.test?.title, HomePage.wishlistIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('wishlist icon should not be present in mobile view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.wishlistIcon, 'MOBILE', this.test?.title, HomePage.wishlistIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    });
+
+    describe('gridview icon element checks', () => {
+        it('gridview icon should be present in desktop view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.gridViewIcon, 'DESKTOP', this.test?.title, HomePage.gridviewIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('gridview icon should not be present in tablet view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.gridViewIcon, 'TABLET', this.test?.title, HomePage.gridviewIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('gridview icon should not be present in mobile view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.gridViewIcon, 'MOBILE', this.test?.title, HomePage.gridviewIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    });
+    describe('listview icon element checks', () => {
+        it('listview icon should be present in desktop view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.listViewIcon, 'DESKTOP', this.test?.title, HomePage.listviewIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('listview icon should not be present in tablet view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.listViewIcon, 'TABLET', this.test?.title, HomePage.listviewIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('listview icon should not be present in mobile view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.listViewIcon, 'MOBILE', this.test?.title, HomePage.listviewIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    });
+
+    describe('open filters icon element checks', () => {
+        it('open filters icon should not be present in desktop view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.openFiltersIcon, 'DESKTOP', this.test?.title, HomePage.openFiltersIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('open filters icon should be present in tablet view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.openFiltersIcon, 'TABLET', this.test?.title, HomePage.openFiltersIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('open filters icon should be present in mobile view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.openFiltersIcon, 'MOBILE', this.test?.title, HomePage.openFiltersIconLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    });
+
+    describe('filters label element checks', () => {
+        it('filters label should not be present in desktop view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.filtersLabel, 'DESKTOP', this.test?.title, HomePage.filtersLabelLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('filters label should be present in tablet view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.filtersLabel, 'TABLET', this.test?.title, HomePage.filtersLabelLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('listview icon should not be present in mobile view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.filtersLabel, 'MOBILE', this.test?.title, HomePage.filtersLabelLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    });
+
+    describe('product shortcuts element checks', () => {
+        it('product shortcuts should not be present in desktop view', async function () {
+            let expectedResult = false;
+            let result = await checkForElementVisibility(HomePage.productShortcuts, 'DESKTOP', this.test?.title, HomePage.productShortcutsLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('product shortcuts should be present in tablet view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.productShortcuts, 'TABLET', this.test?.title, HomePage.productShortcutsLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+        it('product shortcuts should be present in mobile view', async function () {
+            let expectedResult = true;
+            let result = await checkForElementVisibility(HomePage.productShortcuts, 'MOBILE', this.test?.title, HomePage.productShortcutsLocator, expectedResult);
+
+            expect(result).to.equal(true);
+        });
+    });
 });
 
 async function checkForElementVisibility(elementName: any, viewport: string, testName: any, locator: string, expectedResult: boolean) {
@@ -65,7 +210,7 @@ async function checkForElementVisibility(elementName: any, viewport: string, tes
     await browser.setWindowSize(viewportSize.width, viewportSize.height);
 
     let element = await elementName;
-    let isElementVisibleInViewport = await element.isDisplayedInViewport();
+    let isElementVisibleInViewport = await element.isDisplayed();
 
     hackathonReporter(taskNumber,testName,locator,viewport,isElementVisibleInViewport==expectedResult);
 
