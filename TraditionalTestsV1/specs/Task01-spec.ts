2dea2179648f8e076338ae09f403ab62981aae70
James Espie <jamesespie@faceme.com>
2020-07-07 15:04:23 +1200

    Update test to include multiple viewports

--- TraditionalTestsV1/specs/Task01-spec.ts
+++ TraditionalTestsV1/specs/Task01-spec.ts
@@ -2,10 +2,18 @@
 import HomePage from '../pages/homepage'
 const { expect } = require('chai');
 
+const viewportSizes = {
+    DESKTOP: { width: 1200,height: 700 },
+    TABLET: { width: 768,height: 700},
+    MOBILE: { width: 500, height: 700}
+}
+
 describe('Open Homepage', () => {
     HomePage.open();
     it('search bar should be present', async () => {
+        await browser.setWindowSize(viewportSizes.DESKTOP.width, viewportSizes.DESKTOP.height);
+        browser.capabilities
         let searchBar = await HomePage.applifashionSearchBar;
-        expect(await searchBar.isExisting()).to.equal(true);
+        expect(await searchBar.isDisplayedInViewport()).to.equal(true);
     });
 });
