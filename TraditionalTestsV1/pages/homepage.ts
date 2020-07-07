7138af32b6b60e1687a06b0e2c9836cbcd79855b
James Espie <jamesespie@faceme.com>
2020-07-07 19:52:00 +1200

    Extend test to multiple viewports and add generic function to check element presence

--- TraditionalTestsV1/pages/homepage.ts
+++ TraditionalTestsV1/pages/homepage.ts
@@ -4,8 +4,9 @@ class HomePage extends BasePage {
     open() {
         super.open('gridHackathonV1.html');
     }
-    get applifashionHeaderImage() { return $('#IMG____9')}
-    get applifashionSearchBar() { return $('#INPUTtext____42')}
+    get applifashionHeaderImage() { return $('#IMG____9') }
+    get applifashionSearchBar() { return $('#INPUTtext____42') }
+    get applifashionHeaderSubMenu() { return $('#A__showsubmen__23') }
 }
 
 export default new HomePage();
--- TraditionalTestsV1/specs/Task01-spec.ts
+++ TraditionalTestsV1/specs/Task01-spec.ts
@@ -1,5 +1,6 @@
 
 import HomePage from '../pages/homepage'
+import { Browser } from 'webdriverio';
 const { expect } = require('chai');
 
 const viewportSizes = {
@@ -10,10 +11,35 @@ const viewportSizes = {
 
 describe('Open Homepage', () => {
     HomePage.open();
-    it('search bar should be present', async () => {
-        await browser.setWindowSize(viewportSizes.DESKTOP.width, viewportSizes.DESKTOP.height);
-        browser.capabilities
-        let searchBar = await HomePage.applifashionSearchBar;
-        expect(await searchBar.isDisplayedInViewport()).to.equal(true);
+    describe('search bar element checks',() => {
+        it('search bar should be present in desktop view', async () => {
+            expect(await isElementPresentInViewport(HomePage.applifashionSearchBar,viewportSizes.DESKTOP.width,viewportSizes.DESKTOP.height));
+        });
+        it('search bar should be present in tablet view', async () => {
+            expect(await isElementPresentInViewport(HomePage.applifashionSearchBar,viewportSizes.TABLET.width, viewportSizes.TABLET.height)).to.equal(true);
+        });
+        it('search bar should not be present in mobile view', async () => {
+            expect(await isElementPresentInViewport(HomePage.applifashionSearchBar,viewportSizes.MOBILE.width, viewportSizes.MOBILE.height)).to.equal(false);
+        });
     });
+
+    describe('submenu element checks',() => {
+        it('submenu should be present in desktop view', async () => {
+            expect(await isElementPresentInViewport(HomePage.applifashionHeaderSubMenu,viewportSizes.DESKTOP.width,viewportSizes.DESKTOP.height)).to.equal(true);
+        });
+        it('submenu should not be present in desktop view', async () => {
+            expect(await isElementPresentInViewport(HomePage.applifashionHeaderSubMenu, viewportSizes.TABLET.width, viewportSizes.TABLET.height)).to.equal(false);
+        });
+        it('submenu should not be present in desktop view', async () => {
+            expect(await isElementPresentInViewport(HomePage.applifashionHeaderSubMenu, viewportSizes.MOBILE.width, viewportSizes.MOBILE.height)).to.equal(false);
+        });
+    })
+
 });
+
+async function isElementPresentInViewport(elementName: any, viewportWidth: number, viewportHeight: number) {
+    await browser.setWindowSize(viewportWidth, viewportHeight);
+    let element = await elementName;
+    let isElementVisibleInViewport = await element.isDisplayedInViewport();
+    return isElementVisibleInViewport;
+}
